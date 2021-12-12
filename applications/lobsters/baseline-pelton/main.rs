8ea50278be4c3688232b8f01542410b89ed533ee
ishanSharma07 <mailmeishan007@gmail.com>
2021-12-12 18:53:26 +0000

    Quote bug fix

--- applications/lobsters/baseline-pelton/main.rs
+++ applications/lobsters/baseline-pelton/main.rs
@@ -240,7 +240,7 @@ impl Service<TrawlerRequest> for MysqlTrawler {
                         let (mut c, user) = c
                             .first_exec::<_, _, my::Row>(
                                 "SELECT 1 AS `one` FROM users WHERE users.PII_username = ?",
-                                (format!("'user{}'", acting_as.unwrap()),),
+                                (format!("user{}", acting_as.unwrap()),),
                             )
                             .await?;
 
